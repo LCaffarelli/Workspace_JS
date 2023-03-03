b9d185817246ee3db6f8ff9b668394094bfe72f8
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-03-03 15:42:53 +0100

    add demoAngular

---
+++ demoAngular/src/app/en-tete/en-tete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //le selector est le nom de la balise que l'on va utiliser
+  //doit commencer par app- (fait partie de la convention)
+  selector: 'app-en-tete',
+  templateUrl: './en-tete.component.html',
+  styleUrls: ['./en-tete.component.css']
+})
+export class EnTeteComponent {
+
+}
